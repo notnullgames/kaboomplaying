3a1109fe915cdd5d8349bbd6032caf8c8a5a5766
David Konsumer <konsumer@jetboystudio.com>
2021-07-23 22:09:13 -0700

    more compact

--- src/maps/level1.js
+++ src/maps/level1.js
@@ -9,7 +9,7 @@ const floor = `
 ░░░░░░░░░░
 ░░░░░░░░░░
 ░░░░░░░░░░
-`
+`;
 
 const walls = `
 ▛▀▀▀▀▀▀▀▀▜
@@ -20,53 +20,24 @@ const walls = `
 ▌        ▐
 ▌        ▐
 ▙▄▄▄▄▄▄▄▄▟
-`
+`;
 
-export default function level1 ({ loadSprite, sprite, solid, addLevel }) {
-  loadSprite('terrain', 'terrain.png', { sliceX: 6, sliceY: 59 })
+export default function level1({ loadSprite, sprite, solid, addLevel }) {
+  loadSprite("terrain", "terrain.png", { sliceX: 6, sliceY: 59 });
 
   const terrain = {
-    '░': [
-      sprite('terrain', { frame: 22 })
-    ],
-    '█': [
-      sprite('terrain', { frame: 20 }),
-      solid()
-    ],
-    '▛': [
-      sprite('terrain', { frame: 40 }),
-      solid()
-    ],
-    '▜': [
-      sprite('terrain', { frame: 41 }),
-      solid()
-    ],
-    '▙': [
-      sprite('terrain', { frame: 45 }),
-      solid()
-    ],
-    '▟': [
-      sprite('terrain', { frame: 45 }),
-      solid()
-    ],
-    '▀': [
-      sprite('terrain', { frame: 45 }),
-      solid()
-    ],
-    '▄': [
-      sprite('terrain', { frame: 64 }),
-      solid()
-    ],
-    '▌': [
-      sprite('terrain', { frame: 53 }),
-      solid()
-    ],
-    '▐': [
-      sprite('terrain', { frame: 51 }),
-      solid()
-    ]
-  }
+    "░": [sprite("terrain", { frame: 22 })],
+    "█": [sprite("terrain", { frame: 20 }), solid()],
+    "▛": [sprite("terrain", { frame: 40 }), solid()],
+    "▜": [sprite("terrain", { frame: 41 }), solid()],
+    "▙": [sprite("terrain", { frame: 45 }), solid()],
+    "▟": [sprite("terrain", { frame: 45 }), solid()],
+    "▀": [sprite("terrain", { frame: 45 }), solid()],
+    "▄": [sprite("terrain", { frame: 64 }), solid()],
+    "▌": [sprite("terrain", { frame: 53 }), solid()],
+    "▐": [sprite("terrain", { frame: 51 }), solid()],
+  };
 
-  addLevel(floor.trim().split('\n'), { width: 32, height: 32, ...terrain })
-  addLevel(walls.trim().split('\n'), { width: 32, height: 32, ...terrain })
+  addLevel(floor.trim().split("\n"), { width: 32, height: 32, ...terrain });
+  addLevel(walls.trim().split("\n"), { width: 32, height: 32, ...terrain });
 }
